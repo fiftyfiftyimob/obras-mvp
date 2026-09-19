22bcfff191bf87169aaaffc2610ee03f4138b3a0
fiftyfiftyimob <fiftyfiftyimob@gmail.com>
2026-09-18 23:21:38 -0300

    feat: registra UsuariosModule e AuthModule em app.module.ts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { AppController } from './app.controller';
+import { UsuariosModule } from './usuarios/usuarios.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, UsuariosModule, AuthModule],
   controllers: [AppController],
   providers: [],
 })
