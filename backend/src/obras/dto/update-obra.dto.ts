472faf05fdb415b6289e7554f065646d106e50b4
fiftyfiftyimob <fiftyfiftyimob@gmail.com>
2026-09-19 07:59:09 -0300

    fix: remove dependencia ausente do dto de obras

--- backend/src/obras/dto/update-obra.dto.ts
+++ backend/src/obras/dto/update-obra.dto.ts
@@ -1,4 +1,33 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateObraDto } from './create-obra.dto';
+import { IsOptional, IsString, Length, Matches } from 'class-validator';
 
-export class UpdateObraDto extends PartialType(CreateObraDto) {}
+export class UpdateObraDto {
+  @IsOptional()
+  @IsString()
+  @Length(2, 150)
+  nome?: string;
+
+  @IsOptional()
+  @IsString()
+  @Length(2, 255)
+  endereco?: string;
+
+  @IsOptional()
+  @IsString()
+  @Length(2, 100)
+  cidade?: string;
+
+  @IsOptional()
+  @IsString()
+  @Matches(/^[A-Za-z]{2}$/)
+  estado?: string;
+
+  @IsOptional()
+  @IsString()
+  @Length(2, 150)
+  clienteNome?: string;
+
+  @IsOptional()
+  @IsString()
+  @Length(3, 100)
+  clienteContato?: string;
+}
