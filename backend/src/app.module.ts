d24d3542c10e74684357b5a88e2c32bd4d4befb1
fiftyfiftyimob <fiftyfiftyimob@gmail.com>
2026-09-18 23:15:02 -0300

    fix: remove imports de modulos inexistentes em app.module.ts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,29 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AuthModule } from './auth/auth.module';
-import { UsuariosModule } from './usuarios/usuarios.module';
-import { ObrasModule } from './obras/obras.module';
-import { FrentesModule } from './frentes/frentes.module';
-import { ServicosModule } from './servicos/servicos.module';
-import { EquipesModule } from './equipes/equipes.module';
-import { ColaboradoresModule } from './colaboradores/colaboradores.module';
-import { TarefasModule } from './tarefas/tarefas.module';
-import { EvolucoesModule } from './evolucoes/evolucoes.module';
-import { LocalizacoesModule } from './localizacoes/localizacoes.module';
-import { RdosModule } from './rdos/rdos.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { AppController } from './app.controller';
 
 @Module({
-  imports: [
-    AuthModule,
-    UsuariosModule,
-    ObrasModule,
-    FrentesModule,
-    ServicosModule,
-    EquipesModule,
-    ColaboradoresModule,
-    TarefasModule,
-    EvolucoesModule,
-    LocalizacoesModule,
-    RdosModule,
-  ],
+  imports: [PrismaModule],
+  controllers: [AppController],
+  providers: [],
 })
 export class AppModule {}
