79e633300ec751a061301aa4a139bbf255c57d92
fiftyfiftyimob <fiftyfiftyimob@gmail.com>
2026-09-18 23:36:24 -0300

    fix: adiciona validacao ao CreateUsuarioDto

--- backend/src/usuarios/dto/create-usuario.dto.ts
+++ backend/src/usuarios/dto/create-usuario.dto.ts
@@ -1,7 +1,22 @@
+import { IsEmail, IsIn, IsNotEmpty, IsOptional, IsString, MinLength } from 'class-validator';
+
 export class CreateUsuarioDto {
+  @IsString()
+  @IsNotEmpty()
   nome: string;
+
+  @IsString()
+  @IsNotEmpty()
   telefone: string;
+
+  @IsEmail()
+  @IsOptional()
   email?: string;
+
+  @IsString()
+  @MinLength(6)
   senha: string;
+
+  @IsIn(['gestor', 'estagiario', 'operario'])
   perfil: 'gestor' | 'estagiario' | 'operario';
 }
