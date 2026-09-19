dc2738fa80f435335484b667cd10dba3238a87e5
fiftyfiftyimob <fiftyfiftyimob@gmail.com>
2026-09-18 23:41:33 -0300

    fix: adiciona validacao ao LoginDto

--- backend/src/auth/dto/login.dto.ts
+++ backend/src/auth/dto/login.dto.ts
@@ -1,4 +1,11 @@
+import { IsNotEmpty, IsString, MinLength } from 'class-validator';
+
 export class LoginDto {
+  @IsString()
+  @IsNotEmpty()
   telefone: string;
+
+  @IsString()
+  @MinLength(6)
   senha: string;
 }
